20560fecbfa67bbad187452a9ef7acbdd0c4b8e4
Gili Yankovitch <giliy@nyxsecurity.net>
2021-01-18 16:31:37 -0800

    Flipping color wheels in JS View

--- app/static/script.js
+++ app/static/script.js
@@ -92,13 +92,13 @@ function createTriangles(num)
 	var height = Math.sqrt(Math.pow(side, 2) - Math.pow(side/2, 2))
 
 	// Calculate the gradient
-	var colorStart = $('#picker0').tinycolorpicker().data('plugin_tinycolorpicker').colorHex;
+	var colorStart = $('#picker1').tinycolorpicker().data('plugin_tinycolorpicker').colorHex;
 	if (colorStart == "")
 		colorStart = "#ffffff";
 
 	colorStart = hexToRGB(colorStart);
 
-	var colorEnd = $('#picker1').tinycolorpicker().data('plugin_tinycolorpicker').colorHex;
+	var colorEnd = $('#picker0').tinycolorpicker().data('plugin_tinycolorpicker').colorHex;
 	if (colorEnd == "")
 		colorEnd = "#ffffff";
 
@@ -211,8 +211,8 @@ $(document).ready(function() {
 		createTriangles(NUM_TRIANGLES);
 	});
 
-	$('#picker0')[0].style.marginRight = "50px";
-	$('#picker1')[0].style.marginLeft = "50px";
+	$('#picker1')[0].style.marginRight = "50px";
+	$('#picker0')[0].style.marginLeft = "50px";
 
 	$("input[name='animation']").change(function (event) {
 		createTriangles(NUM_TRIANGLES);
