715e2f553c1232cb6300d4f5f954c0e1faa100a1
Gili Yankovitch <giliy@nyxsecurity.net>
2021-05-02 02:02:19 +0300

    Update breathing.py

--- app/filters/breathing.py
+++ app/filters/breathing.py
@@ -2,7 +2,7 @@
 from app.animation import Animation
 
 def init(colors):
-	animation = Animation(colors, 15, 40, 750)
+	animation = Animation(colors, 31, 40, 750)
 
 	initFrame = animation.createFrame()
 
@@ -13,6 +13,6 @@ def init(colors):
 def frame(animation):
 	nextFrame = animation.createFrame()
 
-	nextFrame.addBrightness(-1 if animation.currentFrameNum() > 8 else 1)
+	nextFrame.addBrightness(-1 if animation.currentFrameNum() > 16 else 1)
 
 	return animation
